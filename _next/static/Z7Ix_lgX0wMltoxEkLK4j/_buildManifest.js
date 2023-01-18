8f15003b660058cdfb6d546d642e0d690c82f7ba
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2023-01-18 23:45:00 +0900

    Updates

---
+++ _next/static/Z7Ix_lgX0wMltoxEkLK4j/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-8619d3a1a46e5850.js","static/css/c1d019d22a18277e.css","static/chunks/pages/index-d240715687b72aec.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
